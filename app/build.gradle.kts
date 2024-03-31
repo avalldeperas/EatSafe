0409c2d523eb483c7a151947765b9dba195e15e0
Albert Valldeperas <albertvalldeperas@gmail.com>
2024-03-31 12:54:46 +0200

    avb | PAC-2 | upgrades compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.hilt.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -69,7 +70,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
 }
 
